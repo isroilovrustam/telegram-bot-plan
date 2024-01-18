e9addf97e262f6901b82d619cdb578b1faf6eaa8
Rustam <rustamjonisroilov213@gmail.com>
2024-01-18 22:57:37 +0500

    removkeyboard

--- handlers/users/cources_handlers.py
+++ handlers/users/cources_handlers.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from keyboards.default.start_keyboard import cources_keyboard, start_keyboards
 from loader import dp
+from aiogram.types import ReplyKeyboardRemove
 from keyboards.default.cources_keyboard import python_keyboard
 
 
@@ -12,6 +13,10 @@ async def cources_handler(message: types.Message):
 @dp.message_handler(text="Python")
 async def cources_handler(message: types.Message):
     await message.answer("Darslar: ", reply_markup=python_keyboard)
+
+@dp.message_handler(text="#1-dars")
+async def cources_handler(message: types.Message):
+    await message.answer("Darsni ko'rishingiz mumkin: ", reply_markup=ReplyKeyboardRemove())
 
 
 @dp.message_handler(text="Boshiga")
